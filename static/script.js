ee4118c9c04390d7712f546c58fa6f7ac7ce1a94
NersuVenkataSaiSravani <ammalusravani@gmail.com>
2026-08-14 13:02:45 +0530

    Upload full stack Flask application

---
+++ static/script.js
@@ -0,0 +1,7 @@
+function submitForm() {
+
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+
+    alert("Name: " + name + "\nAge: " + age);
+}
