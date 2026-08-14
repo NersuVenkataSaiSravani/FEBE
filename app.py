b4f6b40ea777f148307fdcb903ad602f709eeabf
NersuVenkataSaiSravani <147939468+NersuVenkataSaiSravani@users.noreply.github.com>
2026-08-14 13:15:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-=======
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello from Flask"
-
-if __name__ == "__main__":
->>>>>>> 0a8ad19be8db1d2bceee513c32e6c66a5e76f76f
     app.run(host="0.0.0.0", port=5000)
